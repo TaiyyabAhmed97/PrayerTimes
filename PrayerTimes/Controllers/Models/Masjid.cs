d631767a56ce17672d3619fbb1c14a5275babd36
Taiyyab <hafiztaiyyab11@gmail.com>
2017-09-09 12:37:43 -0500

    Added the layout of cpt.fdevs.com deleted how i was redering the models on the Home Index View before and used the way that the V1 used Finished issue 3

--- PrayerTimes/Controllers/Models/Masjid.cs
+++ PrayerTimes/Controllers/Models/Masjid.cs
@@ -16,22 +16,22 @@ namespace PrayerTimes.Models
 
         public string PHnum { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}")]
         public DateTime Fajr { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}")]
         public DateTime Zuhr { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}")]
         public DateTime Asr { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}")]
         public DateTime Maghrib { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}")]
         public DateTime Isha { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}")]
         public DateTime Jummah_1 { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:HH:mm}")]
